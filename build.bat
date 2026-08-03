6b9e61fe4569635088ba563ef8b905f322fc9b35
Marek <vadim_borodulin21@mail.ru>
2026-08-03 14:32:31 +0300

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Building code.c ...
+
+gcc -c src/main.c -o build/main.o -Iinclude
+gcc -c src/init.c -o build/init.o -Iinclude
+gcc -c src/game.c -o build/game.o -Iinclude
+gcc -c src/character.c -o build/character.o -Iinclude
+gcc -c src/combat.c -o build/combat.o -Iinclude
+
+gcc build/main.o build/init.o build/character.o build/game.o build/combat.o -o bin/main.exe
+
+echo Build complete!
+
+pause
